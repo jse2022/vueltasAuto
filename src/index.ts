6dcc81aad5978744df5bbb51b7fe6d783f45989f
jse2022 <104039883+jse2022@users.noreply.github.com>
2022-04-29 20:23:45 -0300

    Vueltas del auto Describo paso a paso promedio de tiempo de vueltas

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
 import "./styles.css";
+const cantidadDeVueltas: number = 4;
+let vueltaUno: number = Number(prompt("Ingrese vuelta 1"));
+let vueltaDos: number = Number(prompt("Ingrese vuelta 2"));
+let vueltaTres: number = Number(prompt("Ingrese vuelta 3"));
+let vueltaCuatro: number = Number(prompt("Ingrese vuelta 4"));
+let promedioVueltas =
+  (vueltaUno + vueltaDos + vueltaTres + vueltaCuatro) / cantidadDeVueltas;
+alert("El promedio de vueltas es: " + promedioVueltas);
