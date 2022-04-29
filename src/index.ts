a6336fb4099660d6c9476961c75a5056a80098d3
jse2022 <104039883+jse2022@users.noreply.github.com>
2022-04-29 20:26:44 -0300

    Modifico modifico las variables

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import "./styles.css";
 const cantidadDeVueltas: number = 4;
-let vueltaUno: number = Number(prompt("Ingrese vuelta 1"));
-let vueltaDos: number = Number(prompt("Ingrese vuelta 2"));
-let vueltaTres: number = Number(prompt("Ingrese vuelta 3"));
-let vueltaCuatro: number = Number(prompt("Ingrese vuelta 4"));
+let tiempoVueltaUno: number = Number(prompt("Ingrese tiempo de vuelta 1"));
+let tiempoVueltaDos: number = Number(prompt("Ingrese tiempo de vuelta 2"));
+let tiempoVueltaTres: number = Number(prompt("Ingrese tiempo de vuelta 3"));
+let tiempoVueltaCuatro: number = Number(prompt("Ingrese tiempo de vuelta 4"));
 let promedioVueltas =
-  (vueltaUno + vueltaDos + vueltaTres + vueltaCuatro) / cantidadDeVueltas;
+  (tiempoVueltaUno + tiempoVueltaDos + tiempoVueltaTres + tiempoVueltaCuatro) /
+  cantidadDeVueltas;
 alert("El promedio de vueltas es: " + promedioVueltas);
